ea1240b5532e051a4ad773b1cf0923dfcac2779e
Siva-2517 <sivasurya6430@gmail.com>
2025-10-14 11:29:48 +0530

    prop commit

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ const sum2=(a,b,c)=>{
     return a+b+c;
 }   
 console.log(sum2(2,3,4));
+
+const mull=(a,b)=>a*b;
+
+const square=a=>a*a;
+
+const print=(n)=>{
+    let s=square(n);
+    console.log("Square is:");
+    console.log(s);
+}
+print(6);
+
+
+console.log("First");
+Promise.resolve().then(console.log("From Promise"));
+setTimeout(()=>console.log("From setTimeout"),2000);
+console.log("Last");
